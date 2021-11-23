89cb817b8b2c72a7d267cd30133bfaf4720c3362
DaniaMunoz <daniamildrevm.99@gmail.com>
2021-11-22 22:03:41 -0500

    EnvioCorrectoCorreo_cliente

--- src/controllers/cliente.controller.ts
+++ src/controllers/cliente.controller.ts
@@ -1,3 +1,4 @@
+import {service} from '@loopback/core/dist/service';
 import {
   Count,
   CountSchema,
@@ -13,11 +14,17 @@ import {
 } from '@loopback/rest';
 import {Cliente} from '../models';
 import {ClienteRepository} from '../repositories';
+import {AutenticacionService} from '../services';
+const fetch = require('node-fetch');
 
 export class ClienteController {
   constructor(
     @repository(ClienteRepository)
     public clienteRepository: ClienteRepository,
+
+    //Importamos
+    @service(AutenticacionService)
+    public servicioAutenticacion: AutenticacionService
   ) { }
 
   @post('/clientes')
@@ -39,8 +46,23 @@ export class ClienteController {
     cliente: Omit<Cliente, 'clienteId'>,
   ): Promise<Cliente> {
 
-    //let p = await this.clienteRepository.create(cliente);
-    return this.clienteRepository.create(cliente);
+    let clave = this.servicioAutenticacion.GenerarClave();
+    let claveCifrada = this.servicioAutenticacion.CifrarClave(clave);
+    cliente.clave = claveCifrada;
+    let p = await this.clienteRepository.create(cliente);
+    //return this.clienteRepository.create(cliente);
+
+
+    //Notificar al usuario
+    let destino = cliente.correo;
+    let asunto = 'Registro en la plataforma';
+    let contenido = `Hola ${cliente.nombres}, su nombre de usuario es: ${cliente.correo} y su contraseña es: ${clave} `;
+    fetch(`http://127.0.0.1:5000/envio-correo?correo_destino=${destino}&asunto=${asunto}&contenido=${contenido}`)
+      .then((data: any) => {
+        console.log(data);
+      })
+    return p;
+
   }
 
   @get('/clientes/count')
